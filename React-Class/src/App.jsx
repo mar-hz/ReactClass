04636f17deddc43045e1df056d7f50c9a58b9918
mar-hz <103940408+mar-hz@users.noreply.github.com>
2025-04-07 15:28:55 -0600

    starter app

--- React-Class/src/App.jsx
+++ React-Class/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Boton from './Boton.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,10 +22,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Boton texto={"lol"} onClick={() => setCount((count) => count + 1)}/>
+
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ React-Class/src/Boton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Boton = ({ texto }) => {
+  return (
+    <button>
+      <h1>{texto}</h1>
+    </button>
+  );
+}
+
+export default Boton
