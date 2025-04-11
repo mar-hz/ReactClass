421f18b3b18cbc7d2d95cce0ee0be5d4cd2c8273
mar-hz <103940408+mar-hz@users.noreply.github.com>
2025-04-11 15:55:40 -0600

    login is now actually a login

--- React-Class/src/vistas/Login.jsx
+++ React-Class/src/vistas/Login.jsx
@@ -1,10 +1,31 @@
+import { Box, Button, TextField } from '@mui/material'
+import { useState } from 'react'
 import React from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const onsubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form onSubmit={onsubmit}>
+      <Box
+        margin ={"auto"}
+        flexDirection={"column"}
+        display={"flex"}
+        width={400}
+        marginTop={"20px"}
+      >
+        <TextField label={"Username"} value={username} onChange={(e) => setUsername(e.target.value)} />
+        <TextField type={"password"} label={"Password"} value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button type={"submit"} variant="contained" onSubmit={onsubmit}>
+          Login
+        </Button>
+      </Box>
+    </form>
   )
 }
 
